1d8210d322311668749d38e17ef3fe0d9a9c5a58
Johannes Donath <johannesd@evil-co.com>
2014-01-10 09:55:38 -0500

    Added a basic command listener interface (for auto discovery).

---
+++ Bot/src/main/java/com/evilco/bot/PlugBot/command/ICommandListener.java
@@ -0,0 +1,8 @@
+package com.evilco.bot.PlugBot.command;
+
+/**
+ * @package com.evilco.bot.PlugBot.command
+ * @author Johannes Donath <johannesd@evil-co.com>
+ * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.org>
+ */
+public interface ICommandListener { }
