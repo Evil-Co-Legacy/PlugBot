20f496b8dbbeb38e273fd9a8a236154e905aad09
Johannes Donath <johannesd@evil-co.com>
2014-01-10 00:01:37 -0500

    Fixed annotation policies.

--- Bot/src/main/java/com/evilco/bot/PlugBot/core/plugin/annotation/Plugin.java
+++ Bot/src/main/java/com/evilco/bot/PlugBot/core/plugin/annotation/Plugin.java
@@ -1,10 +1,17 @@
 package com.evilco.bot.PlugBot.core.plugin.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @package com.evilco.bot.PlugBot.core.plugin.annotation
  * @author Johannes Donath <johannesd@evil-co.com>
  * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.org>
  */
+@Retention (RetentionPolicy.RUNTIME)
+@Target (ElementType.TYPE)
 public @interface Plugin {
 
 	/**
