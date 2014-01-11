96c5f6485c531d52b20d8485e7caa504fbe409c2
Johannes Donath <johannesd@evil-co.com>
2014-01-11 01:39:59 -0500

    Added a basic auto woot plugin.

---
+++ plugins/AutoWoot/src/main/java/com/evilco/PlugBot/plugins/AutoWoot/AutoWootPlugin.java
@@ -0,0 +1,12 @@
+package com.evilco.PlugBot.plugins.AutoWoot;
+
+import com.evilco.bot.PlugBot.core.plugin.AbstractPlugin;
+import com.evilco.bot.PlugBot.core.plugin.annotation.Plugin;
+
+/**
+ * @package PACKAGE_NAME
+ * @author Johannes Donath <johannesd@evil-co.com>
+ * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.org>
+ */
+@Plugin(name = "AutoWoot", description = "Keeps the party going when nobody is there!", version = "1.0.0-SNAPSHOT", author = "Johannes Donath <johannesd@evil-co.com>")
+public class AutoWootPlugin extends AbstractPlugin { }
