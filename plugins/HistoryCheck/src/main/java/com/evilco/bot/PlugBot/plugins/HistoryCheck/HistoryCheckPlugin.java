d4ffa37eba8078853843420ae737be912717d403
Johannes Donath <johannesd@evil-co.com>
2014-01-11 00:29:33 -0500

    Added history check plugin ...

---
+++ plugins/HistoryCheck/src/main/java/com/evilco/bot/PlugBot/plugins/HistoryCheck/HistoryCheckPlugin.java
@@ -0,0 +1,12 @@
+package com.evilco.bot.PlugBot.plugins.HistoryCheck;
+
+import com.evilco.bot.PlugBot.core.plugin.AbstractPlugin;
+import com.evilco.bot.PlugBot.core.plugin.annotation.Plugin;
+
+/**
+ * @package com.evilco.bot.PlugBot.plugins.HistoryCheck
+ * @author Johannes Donath <johannesd@evil-co.com>
+ * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.org>
+ */
+@Plugin (name = "HistoryCheck", description = "Checks played tracks against the history.", version = "1.0.0-SNAPSHOT", author = "Johannes Donath <johannesd@evil-co.com>")
+public class HistoryCheckPlugin extends AbstractPlugin { }
