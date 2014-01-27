a8023e58e21db0fd29309afb86a05081f9ed8114
Johannes Donath <johannesd@evil-co.com>
2014-01-26 21:37:42 -0800

    Added a simple representation for the supported driver types.

---
+++ bot/src/main/java/com/evilco/plug/bot/core/driver/WebDriverType.java
@@ -0,0 +1,11 @@
+package com.evilco.plug.bot.core.driver;
+
+/**
+ * @auhtor Johannes Donath <johannesd@evil-co.com>
+ * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.org>
+ */
+public enum WebDriverType {
+	CHROME,
+	FIREFOX,
+	REMOTE
+}
