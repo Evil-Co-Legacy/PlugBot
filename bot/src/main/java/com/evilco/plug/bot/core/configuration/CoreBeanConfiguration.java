acf11d7196923156679051d3d3c91e69e64e7daa
Johannes Donath <johannesd@evil-co.com>
2014-01-27 08:39:04 -0800

    Updated bean configuration.

--- bot/src/main/java/com/evilco/plug/bot/core/configuration/CoreBeanConfiguration.java
+++ bot/src/main/java/com/evilco/plug/bot/core/configuration/CoreBeanConfiguration.java
@@ -1,6 +1,9 @@
 package com.evilco.plug.bot.core.configuration;
 
 import com.evilco.plug.bot.core.Bot;
+import com.evilco.plug.bot.core.event.EventManager;
+import com.evilco.plug.bot.core.plugin.PluginManager;
+import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,18 +18,49 @@ import org.springframework.context.annotation.Scope;
 @ComponentScan (basePackages = {"com.evilco.plug.bot.core"})
 public class CoreBeanConfiguration {
 
+	@Bean (name = "Bot")
+	@Scope ("singleton")
+	public Bot bot () {
+		return new Bot ();
+	}
+
 	/**
 	 * Constructs a new bot configuration.
 	 * @return
 	 */
 	@Bean
+	@Scope ("singleton")
 	public BotConfiguration configuration () {
 		return BotConfiguration.newInstance ();
 	}
 
-	@Bean (name = "Bot")
-	@Scope ("prototype")
-	public Bot bot () {
-		return new Bot ();
+	/**
+	 * Constructs a new event manager.
+	 * @return
+	 */
+	@Bean
+	@Scope ("singleton")
+	public EventManager eventManager () {
+		return (new EventManager ());
+	}
+
+	/**
+	 * Constructs a new plugin manager.
+	 * @return
+	 */
+	@Bean
+	@Scope ("singleton")
+	public PluginManager pluginManager () {
+		return (new PluginManager ());
+	}
+
+	/**
+	 * Reads the current driver instance.
+	 * @return
+	 */
+	@Bean
+	@Scope ("singleton")
+	public WebDriver driver () {
+		return (this.bot ().getDriver ());
 	}
 }
