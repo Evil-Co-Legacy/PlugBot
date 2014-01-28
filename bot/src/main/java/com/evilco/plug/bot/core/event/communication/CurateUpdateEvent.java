1f471c9b6fd467be73a88f018baff650bbd91dbb
Johannes Donath <johannesd@evil-co.com>
2014-01-28 11:18:30 -0800

    Added a basic curate event.

---
+++ bot/src/main/java/com/evilco/plug/bot/core/event/communication/CurateUpdateEvent.java
@@ -0,0 +1,25 @@
+package com.evilco.plug.bot.core.event.communication;
+
+import com.evilco.plug.bot.core.communication.PageCommunicationAdapter;
+import com.evilco.plug.bot.core.communication.data.User;
+
+/**
+ * @auhtor Johannes Donath <johannesd@evil-co.com>
+ * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.org>
+ */
+public class CurateUpdateEvent extends ApiEvent {
+
+	/**
+	 * Stores the user which curated.
+	 */
+	protected User user;
+
+	/**
+	 *
+	 * @param source
+	 */
+	public CurateUpdateEvent (PageCommunicationAdapter source, User user) {
+		super (source);
+		this.user = user;
+	}
+}
