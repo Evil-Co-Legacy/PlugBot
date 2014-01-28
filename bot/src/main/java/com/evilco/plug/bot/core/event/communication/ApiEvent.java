337b9be4fc72ea25172310de4e06c5ad92079d30
Johannes Donath <johannesd@evil-co.com>
2014-01-28 10:59:39 -0800

    Marked api event as abstract.

--- bot/src/main/java/com/evilco/plug/bot/core/event/communication/ApiEvent.java
+++ bot/src/main/java/com/evilco/plug/bot/core/event/communication/ApiEvent.java
@@ -7,7 +7,7 @@ import org.springframework.context.ApplicationEvent;
  * @auhtor Johannes Donath <johannesd@evil-co.com>
  * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.org>
  */
-public class ApiEvent extends ApplicationEvent {
+public abstract class ApiEvent extends ApplicationEvent {
 
 	/**
 	 * Constructs a new API event.
