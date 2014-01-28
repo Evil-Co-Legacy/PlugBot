1c78894876d03b965c8c60524320355cf3a48b87
Johannes Donath <johannesd@evil-co.com>
2014-01-28 10:57:47 -0800

    Created a basic api event class.

---
+++ bot/src/main/java/com/evilco/plug/bot/core/event/communication/ApiEvent.java
@@ -0,0 +1,27 @@
+package com.evilco.plug.bot.core.event.communication;
+
+import com.evilco.plug.bot.core.communication.PageCommunicationAdapter;
+import org.springframework.context.ApplicationEvent;
+
+/**
+ * @auhtor Johannes Donath <johannesd@evil-co.com>
+ * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.org>
+ */
+public class ApiEvent extends ApplicationEvent {
+
+	/**
+	 * Constructs a new API event.
+	 * @param source
+	 */
+	public ApiEvent (PageCommunicationAdapter source) {
+		super (source);
+	}
+
+	/**
+	 * Returns the event sender.
+	 * @return
+	 */
+	public PageCommunicationAdapter getAdapter () {
+		return ((PageCommunicationAdapter) this.source);
+	}
+}
