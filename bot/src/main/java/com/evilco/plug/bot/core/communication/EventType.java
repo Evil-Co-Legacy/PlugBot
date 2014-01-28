8e8e65f4dad051f5f90d290a8790d207bcdb0f7f
Johannes Donath <johannesd@evil-co.com>
2014-01-28 10:56:26 -0800

    Added plug.dj event types.

---
+++ bot/src/main/java/com/evilco/plug/bot/core/communication/EventType.java
@@ -0,0 +1,25 @@
+package com.evilco.plug.bot.core.communication;
+
+/**
+ * @auhtor Johannes Donath <johannesd@evil-co.com>
+ * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.org>
+ */
+public enum EventType {
+	CHAT,
+	CHAT_COMMAND,
+	CURATE_UPDATE,
+	DJ_ADVANCE,
+	DJ_UPDATE,
+	FAN_JOIN,
+	FRIEND_JOIN,
+	HISTORY_UPDATE,
+	MOD_SKIP,
+	ROOM_SCORE_UPDATE,
+	USER_FAN,
+	USER_JOIN,
+	USER_LEAVE,
+	USER_SKIP,
+	VOTE_SKIP,
+	VOTE_UPDATE,
+	WAIT_LIST_UPDATE
+}
