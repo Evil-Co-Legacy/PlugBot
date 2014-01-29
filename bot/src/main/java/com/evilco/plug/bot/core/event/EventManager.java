7f11ad6d2fc9f86b89b844286b41acbad1a94701
Johannes Donath <johannesd@evil-co.com>
2014-01-28 20:18:55 -0800

    Added logging code.

--- bot/src/main/java/com/evilco/plug/bot/core/event/EventManager.java
+++ bot/src/main/java/com/evilco/plug/bot/core/event/EventManager.java
@@ -5,12 +5,19 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.stereotype.Component;
 
+import java.util.logging.Logger;
+
 /**
  * @auhtor Johannes Donath <johannesd@evil-co.com>
  * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.org>
  */
 @Component
 public class EventManager implements ApplicationEventPublisherAware {
+
+	/**
+	 * Stores the internal logging instance.
+	 */
+	protected static final Logger logger = Logger.getLogger ("EventManager");
 
 	/**
 	 * Stores the application event publisher.
@@ -22,6 +29,9 @@ public class EventManager implements ApplicationEventPublisherAware {
 	 * @param event
 	 */
 	public void fireEvent (ApplicationEvent event) {
+		logger.finest ("Dispatching event of type " + event.getClass ().getCanonicalName () + ".");
+
+		// fire
 		this.publisher.publishEvent (event);
 	}
 
