3d746e7a39771e0a0ebc11db9f98875fb9d7f000
Johannes Donath <johannesd@evil-co.com>
2014-01-29 15:01:40 -0800

    Created a simple command sender for users.

---
+++ bot/src/main/java/com/evilco/plug/bot/core/communication/data/UserCommandSender.java
@@ -0,0 +1,42 @@
+package com.evilco.plug.bot.core.communication.data;
+
+import com.evilco.plug.bot.core.command.ICommandSender;
+import com.evilco.plug.bot.core.communication.PageCommunicationAdapter;
+
+import java.lang.reflect.Field;
+
+/**
+ * @auhtor Johannes Donath <johannesd@evil-co.com>
+ * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.org>
+ */
+public class UserCommandSender extends User implements ICommandSender {
+
+	/**
+	 * Stores the parent communication adapter.
+	 */
+	protected final PageCommunicationAdapter adapter;
+
+	/**
+	 * Constructs a new User Command Sender.
+	 * @param adapter
+	 * @param user
+	 */
+	public UserCommandSender (PageCommunicationAdapter adapter, User user) {
+		for (Field field : user.getClass ().getFields ()) {
+			try {
+				field.setAccessible (true);
+				field.set (this, field.get (user));
+			} catch (IllegalAccessException ignore) { }
+		}
+
+		this.adapter = adapter;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void sendMessage (String message) {
+		this.adapter.sendChat ("@" + this.username + " " + message);
+	}
+}
