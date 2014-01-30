cda19caac15cc8d0b80f9f66666b6cc55295d2ed
Johannes Donath <johannesd@evil-co.com>
2014-01-29 16:29:26 -0800

    Added missing settings for annotations.

--- bot/src/main/java/com/evilco/plug/bot/core/command/annotation/Command.java
+++ bot/src/main/java/com/evilco/plug/bot/core/command/annotation/Command.java
@@ -2,10 +2,17 @@ package com.evilco.plug.bot.core.command.annotation;
 
 import com.evilco.plug.bot.core.communication.data.PermissionLevel;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @auhtor Johannes Donath <johannesd@evil-co.com>
  * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.org>
  */
+@Retention (RetentionPolicy.RUNTIME)
+@Target (ElementType.TYPE)
 public @interface Command {
 
 	/**
--- bot/src/main/java/com/evilco/plug/bot/core/plugin/annotation/Plugin.java
+++ bot/src/main/java/com/evilco/plug/bot/core/plugin/annotation/Plugin.java
@@ -1,9 +1,16 @@
 package com.evilco.plug.bot.core.plugin.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @auhtor Johannes Donath <johannesd@evil-co.com>
  * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.org>
  */
+@Retention (RetentionPolicy.RUNTIME)
+@Target (ElementType.TYPE)
 public @interface Plugin {
 
 	/**
