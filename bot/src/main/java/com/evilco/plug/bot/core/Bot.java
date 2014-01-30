568df130285caf439bbe421451da0f1b9afbefc3
Johannes Donath <johannesd@evil-co.com>
2014-01-29 22:59:39 -0800

    Removed requirement to define a wrapper interface for wrappers.

--- bot/src/main/java/com/evilco/plug/bot/core/Bot.java
+++ bot/src/main/java/com/evilco/plug/bot/core/Bot.java
@@ -103,7 +103,7 @@ public class Bot implements Runnable, ApplicationContextAware {
 	/**
 	 * Stores the wrapper interface.
 	 */
-	@Autowired
+	@Autowired (required = false)
 	protected IWrapperInterface wrapperInterface;
 
 	/**
