46df125adcd9719ffe2ecdf9c6a5a6eac8f211bc
Johannes Donath <johannesd@evil-co.com>
2014-01-29 22:25:37 -0800

    Added basic logging to the abstract plugin. Made the abstract plugin context aware.

--- bot/src/main/java/com/evilco/plug/bot/core/plugin/AbstractPlugin.java
+++ bot/src/main/java/com/evilco/plug/bot/core/plugin/AbstractPlugin.java
@@ -1,21 +1,33 @@
 package com.evilco.plug.bot.core.plugin;
 
 import com.evilco.plug.bot.core.plugin.annotation.Plugin;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 import java.io.File;
+import java.util.logging.Logger;
 
 /**
  * @auhtor Johannes Donath <johannesd@evil-co.com>
  * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.org>
  */
-public class AbstractPlugin implements IPlugin {
+public class AbstractPlugin implements IPlugin, ApplicationContextAware {
 
 	/**
-	 * Stores the plugin manager.
+	 * Caches the internal application context.
 	 */
-	@Autowired
-	private PluginManager pluginManager;
+	private ApplicationContext context = null;
+
+	private Logger logger = null;
+
+	/**
+	 * Constructs a new plugin.
+	 */
+	public AbstractPlugin () {
+		this.logger = (this.getClass ().isAnnotationPresent (Plugin.class) ? Logger.getLogger (this.getClass ().getAnnotation (Plugin.class).name ()) : Logger.getAnonymousLogger ());
+	}
 
 	/**
 	 * Returns a suggested configuration file.
@@ -38,13 +50,21 @@ public class AbstractPlugin implements IPlugin {
 		if (!this.getClass ().isAnnotationPresent (Plugin.class)) return null;
 
 		// get directory
-		File directory = new File (this.pluginManager.getPluginDirectory (), this.getClass ().getAnnotation (Plugin.class).name ());
+		File directory = new File (((PluginManager) this.context.getBean ("pluginManager")).getPluginDirectory (), this.getClass ().getAnnotation (Plugin.class).name ());
 
 		// create directory
 		if (!directory.exists ()) directory.mkdirs ();
 
 		// done
 		return directory;
+	}
+
+	/**
+	 * Returns the current logger instance.
+	 * @return
+	 */
+	public Logger getLogger () {
+		return this.logger;
 	}
 
 	/**
@@ -58,4 +78,12 @@ public class AbstractPlugin implements IPlugin {
 	 */
 	@Override
 	public void onDisable () { }
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void setApplicationContext (ApplicationContext applicationContext) throws BeansException {
+		this.context = applicationContext;
+	}
 }
